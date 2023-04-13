f380990dbab88eda22d58afe01d70e81bb02f66c
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-13 17:00:55 +0200

    photos

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title1", "https://picsum.photos/id/237/300/200", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title2", "https://picsum.photos/id/126/300/200", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/80/300/200", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/254/300/200", 1);
