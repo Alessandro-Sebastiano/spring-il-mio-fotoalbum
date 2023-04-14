4585bfc0fcec882790cdb52545a3a2e18d37f1fe
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 14:57:01 +0200

    category-model

---
+++ src/main/java/org/project/java/springilmiofotoalbum/model/Category.java
@@ -0,0 +1,31 @@
+package org.project.java.springilmiofotoalbum.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, unique = true)
+    private String type;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
