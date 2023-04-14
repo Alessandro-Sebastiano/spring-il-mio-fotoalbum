186ee4f45cbc77b032d23e3a1b3c5ca285f10553
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 16:36:50 +0200

    review

--- src/main/java/org/project/java/springilmiofotoalbum/service/CategoryService.java
+++ src/main/java/org/project/java/springilmiofotoalbum/service/CategoryService.java
@@ -1,11 +1,13 @@
 package org.project.java.springilmiofotoalbum.service;
 
+import org.project.java.springilmiofotoalbum.exceptions.CategoryNotFoundException;
 import org.project.java.springilmiofotoalbum.model.Category;
 import org.project.java.springilmiofotoalbum.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoryService {
@@ -17,4 +19,24 @@ public class CategoryService {
         return categoryRepository.findAll();
     }
 
+    public Category createCategory(Category formCategory) {
+        Category storeCategory = new Category();
+        storeCategory.setType(formCategory.getType());
+        return categoryRepository.save(storeCategory);
+    }
+
+    public Category editCategory(Integer id, Category formCategory) throws CategoryNotFoundException {
+        Category updateCategory = getById(id);
+        updateCategory.setType(formCategory.getType());
+        return categoryRepository.save(updateCategory);
+    }
+
+    public Category getById(Integer id) throws CategoryNotFoundException {
+        Optional<Category> result = categoryRepository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        } else {
+            throw new CategoryNotFoundException(Integer.toString(id));
+        }
+    }
 }
