81ff970590b83caa91e8b5093b6cb6585e512e40
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 14:57:56 +0200

    category-service

---
+++ src/main/java/org/project/java/springilmiofotoalbum/service/CategoryService.java
@@ -0,0 +1,20 @@
+package org.project.java.springilmiofotoalbum.service;
+
+import org.project.java.springilmiofotoalbum.model.Category;
+import org.project.java.springilmiofotoalbum.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+}
