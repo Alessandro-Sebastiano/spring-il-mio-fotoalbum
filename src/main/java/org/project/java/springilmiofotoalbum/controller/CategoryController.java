8257b378cee272e375f1fec0f3eb2ab7f5351ec3
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 16:37:40 +0200

    category-controller

---
+++ src/main/java/org/project/java/springilmiofotoalbum/controller/CategoryController.java
@@ -0,0 +1,58 @@
+package org.project.java.springilmiofotoalbum.controller;
+
+import jakarta.validation.Valid;
+import org.project.java.springilmiofotoalbum.exceptions.CategoryNotFoundException;
+import org.project.java.springilmiofotoalbum.model.AlertMessages;
+import org.project.java.springilmiofotoalbum.model.Category;
+import org.project.java.springilmiofotoalbum.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public String index(Model model) {
+        List<Category> categories = categoryService.getAllCategories();
+        model.addAttribute("categories", categories);
+        model.addAttribute("category", new Category());
+        return "/categories/index";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("category") Category formCategory, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        boolean errors = bindingResult.hasErrors();
+
+        if (errors) {
+            return "/categories";
+        }
+
+        categoryService.createCategory(formCategory);
+        redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.SUCCESS, "Created category"));
+        return "redirect:/categories";
+    }
+
+    @GetMapping("/edit/{id}")
+    public String edit(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            Category result = categoryService.getById(id);
+            model.addAttribute("category", result);
+            model.addAttribute("categories", categoryService.getAllCategories());
+            return "/categories/index";
+        } catch (CategoryNotFoundException e) {
+            redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.ERROR, "Category not found"));
+        }
+        return "redirect:/categories/index";
+    }
+
+}
