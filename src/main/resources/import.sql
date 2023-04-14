e2f8aa417d0c5cc37d1157ee76da4895af87f94b
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 15:00:31 +0200

    insert-categories-db

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,3 +2,9 @@ INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptio
 INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title2", "https://picsum.photos/id/126/1920/1080", 1);
 INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/80/1920/1080", 1);
 INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/254/1920/1080", 1);
+
+INSERT INTO album_db.categories(`type`) VALUES('Animals');
+INSERT INTO album_db.categories(`type`) VALUES('Sport');
+INSERT INTO album_db.categories(`type`) VALUES('Nature');
+INSERT INTO album_db.categories(`type`) VALUES('Art');
+INSERT INTO album_db.categories(`type`) VALUES('Architectural');
