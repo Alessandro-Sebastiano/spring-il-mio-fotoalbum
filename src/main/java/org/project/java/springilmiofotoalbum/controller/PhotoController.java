a97fe9afb8fb3bc985a28b3f915a36c2bdf92cd8
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 11:18:53 +0200

    alert-redirect-attributes

--- src/main/java/org/project/java/springilmiofotoalbum/controller/PhotoController.java
+++ src/main/java/org/project/java/springilmiofotoalbum/controller/PhotoController.java
@@ -2,15 +2,15 @@ package org.project.java.springilmiofotoalbum.controller;
 
 import jakarta.validation.Valid;
 import org.project.java.springilmiofotoalbum.exceptions.PhotoNotFoundException;
+import org.project.java.springilmiofotoalbum.model.AlertMessages;
 import org.project.java.springilmiofotoalbum.model.Photo;
 import org.project.java.springilmiofotoalbum.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -29,14 +29,15 @@ public class PhotoController {
     }
 
     @GetMapping("{id}")
-    public String show(@PathVariable Integer id, Model model) {
+    public String show(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
         try {
             Photo showPhoto = photoService.getById(id);
             model.addAttribute("photo", showPhoto);
             return "/photo/show";
         } catch (PhotoNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.ERROR, "Photo not found"));
         }
+        return "redirect:/photos";
     }
 
 
@@ -47,7 +48,7 @@ public class PhotoController {
     }
 
     @PostMapping("/create")
-    public String store(@Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, Model model) {
+    public String store(@Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
 
         boolean errors = bindingResult.hasErrors();
 
@@ -56,22 +57,24 @@ public class PhotoController {
         }
 
         photoService.createPhoto(formPhoto);
+        redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.SUCCESS, "Created photo"));
         return "redirect:/photos";
     }
 
     @GetMapping("/edit/{id}")
-    public String edit(@PathVariable Integer id, Model model) {
+    public String edit(@PathVariable Integer id, Model model, RedirectAttributes redirectAttributes) {
         try {
             Photo result = photoService.getById(id);
             model.addAttribute("photo", result);
             return "/photo/edit";
         } catch (PhotoNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.ERROR, "Photo not found"));
         }
+        return "redirect:/photos";
     }
 
     @PostMapping("/edit/{id}")
-    public String update(@PathVariable Integer id, @Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult) {
+    public String update(@PathVariable Integer id, @Valid @ModelAttribute("photo") Photo formPhoto, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         boolean errors = bindingResult.hasErrors();
 
         if (errors) {
@@ -79,15 +82,17 @@ public class PhotoController {
         }
 
         photoService.editPhoto(id, formPhoto);
-        return "redirect:/photos";
+        redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.SUCCESS, "Edited photo"));
+        return "redirect:/photos/" + id;
     }
 
     @GetMapping("/delete/{id}")
-    public String delete(@PathVariable Integer id) {
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
         try {
             photoService.delete(id);
+            redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.SUCCESS, "Deleted photo"));
         } catch (PhotoNotFoundException e) {
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+            redirectAttributes.addFlashAttribute("alert", new AlertMessages(AlertMessages.typeAlert.ERROR, "Photo not deleted, not found"));
         }
         return "redirect:/photos";
     }
