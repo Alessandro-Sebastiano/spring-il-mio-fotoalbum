be6626ae721d3a29bd134b560467179a3abe2156
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 10:13:24 +0200

    review

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,4 +1,4 @@
-INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title1", "https://picsum.photos/id/237/300/200", 1);
-INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title2", "https://picsum.photos/id/126/300/200", 1);
-INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/80/300/200", 1);
-INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/254/300/200", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title1", "https://picsum.photos/id/237/1920/1080", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("description", "title2", "https://picsum.photos/id/126/1920/1080", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/80/1920/1080", 1);
+INSERT INTO album_db.photos(description, title, url, visible) VALUES("descriptiond ", "title3", "https://picsum.photos/id/254/1920/1080", 1);
