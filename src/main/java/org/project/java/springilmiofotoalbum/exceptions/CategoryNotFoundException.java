2a6de548f00d994782f376f6aa3ddf29204d8791
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-14 16:36:32 +0200

    category-exception

---
+++ src/main/java/org/project/java/springilmiofotoalbum/exceptions/CategoryNotFoundException.java
@@ -0,0 +1,9 @@
+package org.project.java.springilmiofotoalbum.exceptions;
+
+public class CategoryNotFoundException extends RuntimeException {
+
+    public CategoryNotFoundException(String message) {
+        super(message);
+    }
+
+}
