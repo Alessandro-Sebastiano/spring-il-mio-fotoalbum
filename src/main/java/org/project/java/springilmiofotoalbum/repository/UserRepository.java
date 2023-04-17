997ff00b8ea863c579bc5ae33140770b79e5882c
Alessandro-Sebastiano <alesebastiano15@gmail.com>
2023-04-17 15:17:29 +0200

    security

---
+++ src/main/java/org/project/java/springilmiofotoalbum/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package org.project.java.springilmiofotoalbum.repository;
+
+import org.project.java.springilmiofotoalbum.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    public Optional<User> findByEmail(String email);
+
+}
---
+++ src/main/java/org/project/java/springilmiofotoalbum/security/DatabaseUserDetailsService.java
@@ -0,0 +1,29 @@
+package org.project.java.springilmiofotoalbum.security;
+
+import org.project.java.springilmiofotoalbum.model.User;
+import org.project.java.springilmiofotoalbum.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class DatabaseUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        Optional<User> user = userRepository.findByEmail(username);
+        if (user.isPresent()) {
+            return new DatabaseUserDetails(user.get());
+        } else {
+            throw new UsernameNotFoundException("User with email " + username + " not found");
+        }
+    }
+}
